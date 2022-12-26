05ab1e2ba078707fcd82d4831b1a19b9adb92bee
Kübra Köse <kbr__1544@hotmail.com>
2022-12-26 21:05:40 +0300

    DOCKER VOLUME  ASP.NET CORE MVC PROJESİ-IMAGESAVE SAYFASI EKLEME  işlemi yapıldı.

--- UdemyDockerize/AspNetCoreMvc/Controllers/HomeController.cs
+++ UdemyDockerize/AspNetCoreMvc/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using AspNetCoreMvc.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,6 +27,32 @@ namespace AspNetCoreMvc.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+        //burda metod oluşturalım
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+        //resim ekle dediğinde basılacak olan metodu da yazalım tabiki text olacağı için async bir şekilde yapabiliriz.Task da ne döneceksek biz ıactionresult döceğiz
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile imageFile)
+        {//if diyoruz imageFile geliyor mu gelmiyor mu kontrol edelim
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                //rastgee üretsin bu yüzden guid diyorum.
+                //Path.GetExtension dediğimde sadece uzantısını al diyoruz
+                //burdaki şu şekilde gelicez örnek
+                //resim1.jpg sadece jpg kısmı alıcak.
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+            }
             return View();
         }
 
